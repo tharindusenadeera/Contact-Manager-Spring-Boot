7efb1cf98782a3343dd548a40268af4b6a7f92b2
Tharindu Senadeera <tharindusenadeera081@gmail.com>
2024-07-30 10:53:00 +0100

    Initial commit

---
+++ src/main/java/com/springboot/contact/manger/ContactMangerApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.contact.manger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactMangerApplication.class, args);
+	}
+
+}
